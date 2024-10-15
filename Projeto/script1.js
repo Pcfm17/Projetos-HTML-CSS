65b2dbbea8fd5937976338294847781354880851
Pcfm17 <paulocesarfmotta17@gmail.com>
2024-10-14 23:31:03 -0300

    Projeto

--- Projeto/script1.js
+++ Projeto/script1.js
@@ -24,6 +24,13 @@ quadrado(440,440,50,50,'transparent','black');
 quadrado(390,440,50,50,'transparent','black');
 quadrado(440,390,50,50,'transparent','black');
 
+quadrado(10,250,50,50,'transparent','darkturquoise');
+quadrado(10,200,50,50,'transparent','darkturquoise');
+
+quadrado(440,225,50,50,'transparent','darkturquoise');
+
+quadrado(200,250,50,50,'transparent','red')
+
 function linha(x,y,w,z,cor_linha){
     ctx.beginPath();
     ctx.lineWidth = 2;
@@ -50,6 +57,8 @@ linha(10,490,490,490,'black')
 linha(10,10,490,10,'black')
 linha(490,10,490,490,'black')
 
+linha(250,250,250,440,'gray')
+
 function arcos(x,y,raio,angulinicial,angulfinal,cor_linha,cor_preencimento){
     ctx.beginPath();
     ctx.lineWidth = 2;
@@ -62,4 +71,47 @@ function arcos(x,y,raio,angulinicial,angulfinal,cor_linha,cor_preencimento){
 }
 
 // arcos
-arcos(250,250,50,0*Math.PI,1*Math.PI,'green','transparent');
+arcos(250,250,100,1*Math.PI,0*Math.PI,'green','transparent');
+arcos(250,490,50,1*Math.PI,0*Math.PI,'green','darkturquoise');
+arcos(220,250,150,1*Math.PI,1.3*Math.PI,'green','transparent');
+arcos(280,250,150,1.7*Math.PI,0*Math.PI,'green','transparent');
+arcos(250,490,130,1*Math.PI,1.5*Math.PI,'green','transparent');
+arcos(250,490,90,1.5*Math.PI,0*Math.PI,'green','transparent');
+
+
+
+// Function to draw a circle
+function drawCircle(x, y, radius, fillColor, borderColor, borderWidth) {
+    ctx.beginPath();
+    ctx.arc(x, y, radius, 0, 2 * Math.PI);
+  
+    // Fill the circle
+    ctx.fillStyle = fillColor;
+    ctx.fill();
+  
+    // Draw the border
+    ctx.strokeStyle = borderColor;
+    ctx.lineWidth = borderWidth;
+    ctx.stroke();
+  }
+//circulo
+drawCircle(110, 360, 20, 'yellow','green', 1);
+drawCircle(350, 360, 20, 'yellow','green', 1);
+drawCircle(250, 185, 20, 'darkturquoise','blue', 1);
+
+
+
+
+
+
+function escrita(x, y, cor_linha, cor_preencimento){
+    ctx.beginPath();
+    ctx.lineWidth = 2;
+    ctx.fillStyle = cor_linha;
+    ctx.font = "35px Arial"
+    ctx.textAlign = "center";
+    ctx.fillText("carvas",250,75);
+    ctx.closePath();
+}
+// texto
+escrita(250,75,'black','transparent')
