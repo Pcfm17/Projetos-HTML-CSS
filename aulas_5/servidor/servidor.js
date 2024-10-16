46f07b90fe806370c3d3a9d3c3cbc5bc0ef93990
Pcfm17 <paulocesarfmotta17@gmail.com>
2024-10-16 11:35:53 -0300

    mudanças feitas

---
+++ aulas_5/servidor/servidor.js
@@ -0,0 +1,13 @@
+require("colors");
+let http = require("http");
+let express = require("express")
+
+let app = express();
+
+app.use(express.static("./public"));
+
+let  server = http.createServer(app)
+
+server.listen(3000)
+
+console.log('Funcionando')
