d84a9576968176b37bb784701b469d647005939a
Pcfm17 <paulocesarfmotta17@gmail.com>
2024-10-22 03:17:08 -0300

    Merge branch 'main' of https://github.com/Pcfm17/Projetos-HTML-CSS

--- aulas_5/servidor/servidor.js
+++ aulas_5/servidor/servidor.js
@@ -1,13 +1,12 @@
-require("colors");
-let http = require("http");
+var http = requirequire("http");
 let express = require("express")
 
-let app = express();
+var app = express();
 
 app.use(express.static("./public"));
 
-let  server = http.createServer(app)
+var server = http.createServer(app);
 
-server.listen(3000)
+server.listen(80)
 
-console.log('Funcionando')
+console.log('Servidor rodando')
